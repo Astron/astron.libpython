ab545fb8941b6f38b5265244df7127fda7b71c1f
Kestred <kestred@riotcave.com>
2014-09-11 23:53:25 -0700

    Small repository changes (.gitignore, __init__.py)

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name = 'astron', version = '0.0', license = 'BSD',
-      description = 'A Python implementation of the application-side of the Astron protocol.'
+      description = 'A Python implementation of the application-side of the Astron protocol.',
       url = 'http://github.com/Astron/astron.libpython',
       packages = ['astron'],
-      platforms = 'any'
+      install_requires = ['bamboo'],
+      platforms = 'any',
       classifiers = [
         'Programming Language :: Python',
         'Development Status :: 2 - Pre-Alpha',
