778757779a19ac1f8fb78c01a5997df2f38d205e
Kestred <kestred@riotcave.com>
2014-09-11 23:39:33 -0700

    Re-organize repository with advice
    Advice from:
    http://www.jeffknupp.com/blog/2013/08/16/open-sourcing-a-python-project-the-right-way/
    
    Leaving simple_example.dc where it is until we figure out where it belongs

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name = 'astron', version = '0.0', license = 'BSD',
+      description = 'A Python implementation of the application-side of the Astron protocol.'
+      url = 'http://github.com/Astron/astron.libpython',
+      packages = ['astron'],
+      platforms = 'any'
+      classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ])
